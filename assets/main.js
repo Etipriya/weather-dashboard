f5740a5688743f54dbfc8e44fa88c89c9d957d9b
Eti Priya <etipriya@gmail.com>
2021-04-06 23:31:45 +0100

    started writing code in js

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+// call renderCities function
+const renderCities = citiesFromLocalStorage => {
+  //for each city, construct a list item and append to the list group
+};
+
+fetchAllWeatherData = cityName => {
+  //construct URL for
+};
+//onload function called on load of the document
+const onLoad = () => {
+  //Read from local storage and store data in variable called citiesFromLocalStorage
+  //if data is present call renderCities and pass the data from local storage
+  //renderCities(citiesFromLocalStorage);
+  //get the last city name from citiesFromLocalStorage and store in variable called cityName
+  //fetchAllWeatherData(cityName)
+};
+
+//function called when the form is submitted
+const onSubmit = () => {};
+
+$("#your-form-id").submit(onSubmit);
+$(document).ready(onLoad);
